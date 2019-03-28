461085a14b8343a8ace47ab6289a430e7edfe6a3
oldtrafford <steve.emmanuelck@gmail.com>
2019-03-28 20:36:37 +0530

    flask migrate files deleted and tables updated

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, Date, Time, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, Time, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from flask_login import UserMixin
 from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.orm.attributes import QueryableAttribute
 
 
 RC_SITE_KEY = '6LdKYnwUAAAAANaFrlhE78bfFmJl2F-ZEPYeaVyh'
@@ -16,6 +15,7 @@ RC_SECRET_KEY = '6LdKYnwUAAAAACt5cz4Qne0eYkJvYNYlmBm97fzI'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:///iconnect.db'
 SECRET_KEY = 'kLRJ7gT64FWASZmwOOJPrFAlUrWFzHCi'
+
 
 class Config(object):
     SECRET_KEY = SECRET_KEY
@@ -36,119 +36,8 @@ bcrypt = Bcrypt(app)
 engine = create_engine('sqlite:///iconnect.db', echo=True)
 Base = declarative_base()
 
-class BaseModel(db.Model):
-    __abstract__ = True
 
-    def to_dict(self, show=None, _hide=[], _path=None):
-        """Return a dictionary representation of this model."""
-
-        show = show or []
-
-        hidden = self._hidden_fields if hasattr(self, "_hidden_fields") else []
-        default = self._default_fields if hasattr(self, "_default_fields") else []
-        default.extend(['id', 'modified_at', 'created_at'])
-
-        if not _path:
-            _path = self.__tablename__.lower()
-
-            def prepend_path(item):
-                item = item.lower()
-                if item.split(".", 1)[0] == _path:
-                    return item
-                if len(item) == 0:
-                    return item
-                if item[0] != ".":
-                    item = ".%s" % item
-                item = "%s%s" % (_path, item)
-                return item
-
-            _hide[:] = [prepend_path(x) for x in _hide]
-            show[:] = [prepend_path(x) for x in show]
-
-        columns = self.__table__.columns.keys()
-        relationships = self.__mapper__.relationships.keys()
-        properties = dir(self)
-
-        ret_data = {}
-
-        for key in columns:
-            if key.startswith("_"):
-                continue
-            check = "%s.%s" % (_path, key)
-            if check in _hide or key in hidden:
-                continue
-            if check in show or key in default:
-                ret_data[key] = getattr(self, key)
-
-        for key in relationships:
-            if key.startswith("_"):
-                continue
-            check = "%s.%s" % (_path, key)
-            if check in _hide or key in hidden:
-                continue
-            if check in show or key in default:
-                _hide.append(check)
-                is_list = self.__mapper__.relationships[key].uselist
-                if is_list:
-                    items = getattr(self, key)
-                    if self.__mapper__.relationships[key].query_class is not None:
-                        if hasattr(items, "all"):
-                            items = items.all()
-                    ret_data[key] = []
-                    for item in items:
-                        ret_data[key].append(
-                            item.to_dict(
-                                show=list(show),
-                                _hide=list(_hide),
-                                _path=("%s.%s" % (_path, key.lower())),
-                            )
-                        )
-                else:
-                    if (
-                        self.__mapper__.relationships[key].query_class is not None
-                        or self.__mapper__.relationships[key].instrument_class
-                        is not None
-                    ):
-                        item = getattr(self, key)
-                        if item is not None:
-                            ret_data[key] = item.to_dict(
-                                show=list(show),
-                                _hide=list(_hide),
-                                _path=("%s.%s" % (_path, key.lower())),
-                            )
-                        else:
-                            ret_data[key] = None
-                    else:
-                        ret_data[key] = getattr(self, key)
-
-        for key in list(set(properties) - set(columns) - set(relationships)):
-            if key.startswith("_"):
-                continue
-            if not hasattr(self.__class__, key):
-                continue
-            attr = getattr(self.__class__, key)
-            if not (isinstance(attr, property) or isinstance(attr, QueryableAttribute)):
-                continue
-            check = "%s.%s" % (_path, key)
-            if check in _hide or key in hidden:
-                continue
-            if check in show or key in default:
-                val = getattr(self, key)
-                if hasattr(val, "to_dict"):
-                    ret_data[key] = val.to_dict(
-                        show=list(show),
-                        _hide=list(_hide),
-                        _path=('%s.%s' % (path, key.lower())),
-                    )
-                else:
-                    try:
-                        ret_data[key] = json.loads(json.dumps(val))
-                    except:
-                        pass
-
-        return ret_data
-
-class Customers(BaseModel):
+class Customers(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     phone_number = Column(String)
@@ -165,6 +54,7 @@ class Customers(BaseModel):
         self.time = time
         self.uuid = uuid
 
+
 class CustomersGrantedEntry(db.Model):
     id = Column(Integer, primary_key=True)
     customer_id = Column(
@@ -173,8 +63,9 @@ class CustomersGrantedEntry(db.Model):
         nullable=False,
         # no need to add index=True, all FKs have indexes
     )
-    customer = db.relationship('Customers', backref='customer_granted_entry')
-    datetime = Column(DateTime)
+    customer = db.relationship('Customers')
+    date = Column(Date)
+    time = Column(Time)
 
 
 class User(UserMixin, db.Model):
